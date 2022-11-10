59e81021b5023f606a8228b11f0354fddf4cf823
Casey Tolentino <casey@Caseys-MacBook-Air.local>
2022-11-10 10:17:21 -0800

    caseys test file

---
+++ testcases.java
@@ -0,0 +1,26 @@
+public class FlashcardsTest {
+    @Test
+
+public void AreFlashcardsEmpty() {
+    @Test
+        Flashcard = new Flashcard();
+        if(questionBank.isEmpty()) 
+        System.out.println("Quiz is not formatted properly and/or empty.")     
+
+    }
+public void AreFlashcardsFormatted() {
+    @Test
+    Flashcard = new Flashcard();
+    /* make sure quiz is formatted properly and can be viewed in flashcard form
+     * issues here would be resolution size of images, file size, file type, 
+     */
+    }
+public void IsUserAtEndofFlashcard() {
+    @Test
+    Flashcard = new Flashcard();
+    /* if flashcard count >=  question count 
+     disable forward() & backward() and return error message
+    */
+    System.out.println("")
+    }
+}
