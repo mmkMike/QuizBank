6aeb4f74d8385fb6c4eb9a23f6162be17c56f8c5
sam <112935537+smh-94@users.noreply.github.com>
2022-11-15 04:13:38 -0800

    new questionBank,Q&A class, removed redunancies

---
+++ questionBank.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+class QuestionBank {
+  public QuestionBank(){
+    ArrayList questionBank = new ArrayList(); 
+  }
+
+  public void viewBank(int i){
+    System.out.println("Debug viewBank method");
+  }
+  public void addQuestion(questionAnswer q){
+    System.out.println("Debug addQuestion");
+  }
+  public void importQuestion(){
+    System.out.println("Debug importQuestion");
+  }
+}
