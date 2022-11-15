69156ff3bd4a35c168962dd48f9239d19c1674be
sam <112935537+smh-94@users.noreply.github.com>
2022-11-15 04:13:38 -0800

    new questionBank,Q&A class, removed redunancies

---
+++ questionAnswer.java
@@ -0,0 +1,17 @@
+class questionAnswer{
+    private String question;
+    private String answer;
+  
+    public void setQuestion(String newQuestion){
+      question = newQuestion;
+    }
+    public void setAnswer(String newAnswer){
+      answer = newAnswer;
+    }
+    public String getQuestion(){
+      return question;
+    }
+    public String getAnswer(){
+      return answer;
+    }
+  }
