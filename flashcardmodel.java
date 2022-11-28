f91e03ba87a2eee0b01b0ff235a1dc5b576a1b38
Casey Tolentino <casey@Caseys-MacBook-Air.local>
2022-11-28 11:31:26 -0800

    Flashcard model for Quiz App

---
+++ flashcardmodel.java
@@ -0,0 +1,21 @@
+package com.example.testmaxflashcards;
+
+public class flashcardModel {
+
+
+    private String front_text;
+    private String back_text;
+
+    public flashcardModel(String front_text, String back_text) {
+        this.front_text = front_text;
+        this.back_text = back_text;
+    }
+    
+    public String getFront_text() {
+        return front_text;
+    }
+
+    public String getBack_text() {
+        return back_text;
+    }
+}
